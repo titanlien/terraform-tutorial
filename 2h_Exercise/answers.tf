24b66b59744589b51961846e8e69dbc973b42c69
Titan Lien <titan.lien@gmail.com>
2019-07-08 23:23:48 +0200

    change instance_type to spot and change region

--- 2h_Exercise/answers.tf
+++ 2h_Exercise/answers.tf
@@ -1,62 +1,74 @@
 provider "aws" {
-  region     = "us-east-1"
+  region     = "us-east-2"
 }
 
 provider "aws" {
-  alias      = "us-west-1"
-  region     = "us-west-1"
+  alias      = "us-west-2"
+  region     = "us-west-2"
 }
 
 variable "us-east-zones" {
-  default = ["us-east-1a", "us-east-1b"]
+  default = ["us-east-2a", "us-east-2b"]
 }
 
 variable "us-west-zones" {
-  default = ["us-west-1a", "us-west-1b"]
+  default = ["us-west-2a", "us-west-2b"]
 }
 
-resource "aws_instance" "west_frontend" {
+variable "instance_type" {
+  default = ["t3a.small", "t3a.micro"]
+}
+
+resource "aws_spot_instance_request" "west_frontend" {
   count             = 2
-  depends_on        = ["aws_instance.west_backend"]
-  provider          = "aws.us-west-1"
-  ami               = "ami-07585467"
+  depends_on        = ["aws_spot_instance_request.west_backend"]
+  provider          = "aws.us-west-2"
+  ami               = "ami-01e24be29428c15b2"
   availability_zone = "${var.us-west-zones[count.index]}"
-  instance_type     = "t2.micro"
+  instance_type     = "${var.instance_type[0]}"
+  spot_price        = "0.03"
+  spot_type         = "one-time"
 
   lifecycle {
     create_before_destroy = true
   }
 }
 
-resource "aws_instance" "frontend" {
+resource "aws_spot_instance_request" "frontend" {
   count             = 2
-  depends_on        = ["aws_instance.backend"]
+  depends_on        = ["aws_spot_instance_request.backend"]
   availability_zone = "${var.us-east-zones[count.index]}"
-  ami               = "ami-66506c1c"
-  instance_type     = "t2.micro"
+  ami               = "ami-02e680c4540db351e"
+  instance_type     = "${var.instance_type[1]}"
+  spot_price        = "0.03"
+  spot_type         = "one-time"
 
   lifecycle {
     create_before_destroy = true
   }
 }
 
-resource "aws_instance" "backend" {
+resource "aws_spot_instance_request" "west_backend" {
+  provider          = "aws.us-west-2"
+  ami               = "ami-01e24be29428c15b2"
   count             = 2
-  availability_zone = "${var.us-east-zones[count.index]}"
-  ami               = "ami-66506c1c"
-  instance_type     = "t2.micro"
+  availability_zone = "${var.us-west-zones[count.index]}"
+  instance_type     = "${var.instance_type[0]}"
+  spot_price        = "0.03"
+  spot_type         = "one-time"
 
   lifecycle {
     prevent_destroy = true
   }
 }
 
-resource "aws_instance" "west_backend" {
-  provider          = "aws.us-west-1"
-  ami               = "ami-07585467"
+resource "aws_spot_instance_request" "backend" {
+  instance_type     = "${var.instance_type[1]}"
   count             = 2
-  availability_zone = "${var.us-west-zones[count.index]}"
-  instance_type     = "t2.micro"
+  availability_zone = "${var.us-east-zones[count.index]}"
+  ami               = "ami-02e680c4540db351e"
+  spot_price        = "0.03"
+  spot_type         = "one-time"
 
   lifecycle {
     prevent_destroy = true
@@ -64,9 +76,9 @@ resource "aws_instance" "west_backend" {
 }
 
 output "frontend_ip" {
-  value = "${aws_instance.frontend.public_ip}"
+  value = "${aws_spot_instance_request.frontend.*.public_ip}"
 }
 
 output "backend_ips" {
-  value = "${aws_instance.backend.*.public_ip}"
+  value = "${aws_spot_instance_request.backend.*.public_ip}"
 }
